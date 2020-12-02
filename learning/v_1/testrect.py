e3b461447c8f08f31a836bd2b751a51319c30b62
Mumu <1248134657@qq.com>
2020-12-02 21:03:42 +0800

    a bit update

--- learning/v_1/testrect.py
+++ learning/v_1/testrect.py
@@ -20,9 +20,16 @@ origin_img = cv2.pyrDown(origin_img)
 
 # 形态学：边缘检测
 img_gray= cv2.cvtColor(origin_img, cv2.COLOR_BGR2GRAY)
-_,Thr_img = cv2.threshold(img_gray,170,255,cv2.THRESH_BINARY)#设定红色通道阈值210（阈值影响梯度运算效果）
+_,Thr_img = cv2.threshold(img_gray,180,255,cv2.THRESH_BINARY)
+_,Thr_img_1 = cv2.threshold(img_gray,110,255,cv2.THRESH_BINARY)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))         #定义矩形结构元素
-bin_img = cv2.morphologyEx(Thr_img, cv2.MORPH_GRADIENT, kernel) #梯度
+bin_img = cv2.morphologyEx(Thr_img-Thr_img_1, cv2.MORPH_GRADIENT, kernel) #梯度
+
+# bin_img = cv2.GaussianBlur(bin_img,(3,3),0)
+# bin_img = cv2.Canny(bin_img, 100, 200)
+
+# 方框滤波
+# bin_img = cv2.boxFilter(bin_img, -1, (3, 3), normalize = 0)
 
 cv2.imwrite("bin_img.png",bin_img)
 
@@ -49,7 +56,7 @@ print(len(contours))
 count = 0
 newContours = []
 for x in contours:
-    if( x.size < 50000 and x.size > 100):
+    if( x.size < 3000 and x.size > 200):
     # if (x.size == 184):
     # if True:
         newContours.append(x)
@@ -62,55 +69,57 @@ lastContours = []
 # 去掉过大或者过小的轮廓
 for c in newContours:
     x,y,w,h = cv2.boundingRect(c)
-    if w*h < 1000000 and w*h > 1000:
+    if w*h < 50000 and w*h > 1000:
+    # if True:
         lastContours.append([x,y,x+w,y+h])
 
 print("len:"+str(len(lastContours)))
 
-# i = 0
-# while(i < len(lastContours)-1):
-#     # j = i + 1
-#     j = 1 if i == 0 else 0
-#     while(j <= len(lastContours)-1):
-#         print("现在i,j为" +str(i)+"," +str(j) )
-#         minx1,miny1,maxx1,maxy1 = lastContours[i]
-#         print(minx1,miny1,maxx1,maxy1)
-#         minx2,miny2,maxx2,maxy2 = lastContours[j]
-#         print("j::::" + str(j))
-#         print(minx2,miny2,maxx2,maxy2)
-#         print("///")
-#         a,b,c,d = mergedRect(minx1,miny1,maxx1,maxy1,minx2,miny2,maxx2,maxy2)
-#         # 判断两个矩形是否可以合并
-#         if a != -1:
-#             print("发现可以合并，现在i,j为" +str(i)+"," +str(j) )
-#             print(a,b,c,d)
-#             print(str(j)+"::"+str(lastContours[j]))
-#             if(j>i):
-#                 lastContours[i]= [a,b,c,d]
-#                 print("删除的为"+str(lastContours.pop(j)))
-#             else:
-#                 lastContours[j]= [a,b,c,d]
-#                 print("删除的为"+str(lastContours.pop(i)))
-#                 i = j
+i = 0
+while(i < len(lastContours)-1):
+    # j = i + 1
+    j = 1 if i == 0 else 0
+    while(j <= len(lastContours)-1):
+        print("现在i,j为" +str(i)+"," +str(j) )
+        minx1,miny1,maxx1,maxy1 = lastContours[i]
+        print(minx1,miny1,maxx1,maxy1)
+        minx2,miny2,maxx2,maxy2 = lastContours[j]
+        print("j::::" + str(j))
+        print(minx2,miny2,maxx2,maxy2)
+        print("///")
+        a,b,c,d = mergedRect(minx1,miny1,maxx1,maxy1,minx2,miny2,maxx2,maxy2)
+        # 判断两个矩形是否可以合并
+        if a != -1:
+            print("发现可以合并，现在i,j为" +str(i)+"," +str(j) )
+            print(a,b,c,d)
+            print(str(j)+"::"+str(lastContours[j]))
+            if(j>i):
+                lastContours[i]= [a,b,c,d]
+                print("删除的为"+str(lastContours.pop(j)))
+            else:
+                lastContours[j]= [a,b,c,d]
+                print("删除的为"+str(lastContours.pop(i)))
+                i = j
 
-#             print("len"+str(len(lastContours)))
-#             # 合并之后需要从头开始遍历
-#             # j = i + 1
-#             j = 1 if i == 0 else 0
-#         else:
-#             print("现在i,j为" +str(i)+"," +str(j) )
-#             if(i == j+1):
-#                 j = i + 1
-#             else:
-#                 j = j + 1
-#     i = i + 1
-# print(len(lastContours))
+            print("len"+str(len(lastContours)))
+            # 合并之后需要从头开始遍历
+            # j = i + 1
+            j = 1 if i == 0 else 0
+        else:
+            print("现在i,j为" +str(i)+"," +str(j) )
+            if(i == j+1):
+                j = i + 1
+            else:
+                j = j + 1
+    i = i + 1
+print(len(lastContours))
 
 
 
 for x in lastContours:
     a,b,c,d = x
     if(c-a)*(d-b) > 2000 and (c-a)/(d-b)<10 and (c-a)/(d-b)>0.1 and (c-a)*(d-b) < 70000:
+    # if True:
         cv2.rectangle(origin_img,(a,b),(c,d),(0,255,0),2)
         cv2.circle(origin_img,((c+a)//2,(d+b)//2),5,(255,0,0), 8)
 
