77b095e0de26e96f8f7ef393f242e27305717074
mumu <1248134657@qq.com>
2020-12-09 19:52:18 +0800

    get necessary Pic

--- learning/v_2/getRoof.py
+++ learning/v_2/getRoof.py
@@ -10,37 +10,38 @@ filename = "roi2"
 # 获取图像
 origin_img = cv2.imread("../core/image/"+filename+".tif")
 
+
+
 # 预处理，将图片缩放
 origin_img = cv2.pyrDown(origin_img)
 
 
-# canny(): 边缘检测
-# img1 = cv2.GaussianBlur(origin_img,(3,3),0)
-# bin_img = cv2.Canny(img1, 100, 200)
-
-# cv2.imshow("origin_img",bin_img)
-# cv2.waitKey(0)
-
 # 形态学：边缘检测
 img_gray = cv2.cvtColor(origin_img, cv2.COLOR_BGR2GRAY)
 
+cv2.imwrite("img/img_gray.png",img_gray)
+
 img_gray = cv2.GaussianBlur(img_gray,(3,3),0)
 
+cv2.imwrite("img/img_gray_Gaussian.png",img_gray)
+
 _,Thr_img = cv2.threshold(img_gray,180,255,cv2.THRESH_BINARY)
+
+cv2.imwrite("img/bin_img.png",Thr_img)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))         #定义矩形结构元素
 bin_img = cv2.morphologyEx(Thr_img, cv2.MORPH_GRADIENT, kernel) #梯度
 
-
-
+cv2.imwrite("img/bin_mor.png",bin_img)
 
 bin_img = cv2.Canny(bin_img, 100, 200)
 
+cv2.imwrite("img/canny.png",bin_img)
 
 # 方框滤波
 bin_img = cv2.boxFilter(bin_img, -1, (3, 3), normalize = 0)
 
-cv2.imwrite("bin_img.png",bin_img)
+cv2.imwrite("img/final_bin_img.png",bin_img)
 
 
 # 提取轮廓
@@ -50,7 +51,8 @@ print(len(contours))
 count = 0
 newContours = []
 for x in contours:
-    if( x.size < 30000 and x.size > 200):
+    if( x.size < 10000 and x.size > 200):
+    # if True:
 
         newContours.append(x)
         count = count + 1
@@ -94,7 +96,6 @@ while(i < len(lastContours)-1):
 
             print("len"+str(len(lastContours)))
             # 合并之后需要从头开始遍历
-            # j = i + 1
             j = 1 if i == 0 else 0
         else:
             print("现在i,j为" +str(i)+"," +str(j) )
@@ -109,10 +110,15 @@ print(len(lastContours))
 
 for x in lastContours:
     a,b,c,d = x
-    if(c-a)*(d-b) > 2000 and (c-a)/(d-b)<10 and (c-a)/(d-b)>0.1 and (c-a)*(d-b) < 70000:
-    # if True:
-        cv2.rectangle(origin_img,(a,b),(c,d),(0,255,0),2)
-        cv2.circle(origin_img,((c+a)//2,(d+b)//2),5,(255,0,0), 8)
+    cv2.drawContours(origin_img,newContours,-1,(0,0,255),3)
 
-cv2.imwrite(filename+"_result.png",origin_img)
+cv2.imwrite("img/"+filename+"_contours.png",origin_img)
+
+for x in lastContours:
+    a,b,c,d = x
+    cv2.rectangle(origin_img,(a,b),(c,d),(0,255,0),2)
+    cv2.circle(origin_img,((c+a)//2,(d+b)//2),5,(255,0,0), 8)
+
+
+cv2.imwrite("img/"+filename+"_result.png",origin_img)
 
